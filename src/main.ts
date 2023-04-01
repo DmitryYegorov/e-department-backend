feaa522018d83b980af2deea6b19c0b3b70b5fd5
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2023-04-01 17:13:34 +0300

    move port to env

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 import { TransformInterceptor } from "./common/interceptors/transform.interceptor";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = app.get(ConfigService);
+
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(3000);
+  await app.listen(config.get("PORT"));
 }
 
 bootstrap();
