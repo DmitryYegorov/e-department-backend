172eee554b723ece53c975048438c316ada30ae3
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2023-04-04 14:16:08 +0300

    created new tables: groups, subjects, classess, students

---
+++ prisma/migrations/20230404111538_create_student_groups_classess_relations/migration.sql
@@ -0,0 +1,65 @@
+/*
+  Warnings:
+
+  - Made the column `middleName` on table `user` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "user" ALTER COLUMN "middleName" SET NOT NULL,
+ALTER COLUMN "lastName" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "subject" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "alis" VARCHAR(255),
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "teacherId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "subject_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "group" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "teacherId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "student" (
+    "id" UUID NOT NULL,
+    "firstName" VARCHAR(255) NOT NULL,
+    "middleName" VARCHAR(255) NOT NULL,
+    "lastName" VARCHAR(255),
+    "groupId" UUID NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "classes" (
+    "id" UUID NOT NULL,
+    "groupId" UUID NOT NULL,
+    "subjectId" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "classes_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "subject" ADD CONSTRAINT "subject_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "group" ADD CONSTRAINT "group_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student" ADD CONSTRAINT "student_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
